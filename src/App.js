7117759e9cf0de6f130e9262d2bfd8de10829cd1
Emelie Lindblom <emelielindblom@Emelies-MacBook-Pro.local>
2022-12-14 17:20:18 +0100

    updated functions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import { ClearButton } from "./ClearButton.js";
 import ListComponent from "./ListComponent.js";
 
 const App = () => {
-  const [circles, setCircles] = useState([]);
+  const [circles, setCircles] = useState([
+    {
+      label: makeid(1),
+      x: 0,
+      y: 0,
+    },
+  ]);
+
+  console.log("circles state", circles);
 
   //checks if there is any previous circles stored in the local storage and if there is it sets it the state
   useEffect(() => {
@@ -20,17 +28,39 @@ const App = () => {
     }
   }, [circles]);
 
-  // Adds a new circle and stores it in local storage
+  // Adds a new circle and should store it in local storage
   function addCircle() {
-    setCircles([...circles, ""]);
-    localStorage.setItem("circles", JSON.stringify(circles));
+    const newCircles = [...circles];
+    newCircles.push({
+      label: makeid(1),
+      x: 0,
+      y: 0,
+    });
+    setCircles([...newCircles]);
+    localStorage.setItem("newCircles", JSON.stringify(newCircles));
   }
+  // this tracks and updates the specific circle with its position
+  // but it needs to be able to find the right circle AND update the circle state with the new positions of X and Y
+  const updateXYPos = (obj) => {
+    console.log(obj);
+  };
 
   //removes all circles when clicking clear button
   function clearContainer() {
     setCircles([]);
     localStorage.clear();
   }
+  // generates a random letter for the circles. extra task to make sure that the letter does not duplicate
+  function makeid(length) {
+    var result = "";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
   return (
     <>
       <div className="external-container">
@@ -42,7 +72,12 @@ const App = () => {
         <div className="container">
           {circles &&
             circles.map((item, index) => (
-              <ListComponent key={index} text={item} />
+              <ListComponent
+                key={index}
+                text={item}
+                onUpdate={updateXYPos}
+                label={item.label}
+              />
             ))}
         </div>
       </div>
--- src/ListComponent.js
+++ src/ListComponent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
 import "./App.css";
 
@@ -6,29 +6,20 @@ const ListComponent = (props) => {
   const [position, setPosition] = useState([{}]);
 
   const index = props.index;
+  const onUpdateFunc = props.onUpdate;
+  const label = props.label;
 
   // keeps track of x and y positions data and saves in the state
   const trackPositions = (data) => {
+    onUpdateFunc({ label, x: data.x, y: data.y });
     setPosition({ x: data.x, y: data.y });
   };
 
-  console.log(position);
-
-  // useEffect(() => {
-  //   const existingDivPositions = JSON.parse(localStorage.getItem("position"));
-  //   setPosition(existingDivPositions);
-  //   console.log(existingDivPositions);
-  // }, []);
-
-  useEffect(() => {
-    localStorage.setItem("position", JSON.stringify(position));
-  }, [position]);
-
   return (
     <Draggable onStop={(e, data) => trackPositions(data)}>
-      <div key={index} index={index} className="Circle">
+      <div key={index} className="Circle">
         <div>
-          <p>x: {position.x}</p> <p>y: {position.y}</p>
+          <p>{label}</p>
         </div>
       </div>
     </Draggable>
